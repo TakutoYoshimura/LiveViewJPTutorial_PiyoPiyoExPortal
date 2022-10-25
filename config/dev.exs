305e58600dfb8be7aa3c9cea9232ad5e3b7f1110
takuto <45676464+TakutoYoshimura@users.noreply.github.com>
2022-10-25 08:20:09 +0000

    configure tailwindcss

--- config/dev.exs
+++ config/dev.exs
@@ -22,7 +22,15 @@ config :phoenix_app, PhoenixAppWeb.Endpoint,
   secret_key_base: "jvuhaPQ/p95ri3RYST7GR6VGYxM1mLVjrTPiAUUqVmYfNfjuMsv9ox189aGw4z5f",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+
   ]
 
 # ## SSL Support
--- replit.nix
+++ replit.nix
@@ -1,6 +1,7 @@
 { pkgs }: {
 	deps = [
-		pkgs.elixir
+		pkgs.nodejs-16_x
+  pkgs.elixir
         pkgs.elixir_ls
         pkgs.sqlite
         pkgs.inotify-tools
