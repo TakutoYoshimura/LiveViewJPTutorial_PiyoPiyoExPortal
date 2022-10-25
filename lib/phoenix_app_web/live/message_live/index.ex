acdb3102d92b83f064728f2f8790f1c49fb67197
takuto <45676464+TakutoYoshimura@users.noreply.github.com>
2022-10-25 10:08:35 +0000

    setting favicon

--- lib/phoenix_app_web/live/message_live/index.ex
+++ lib/phoenix_app_web/live/message_live/index.ex
@@ -28,7 +28,7 @@ defmodule PhoenixAppWeb.MessageLive.Index do
 
   defp apply_action(socket, :index, _params) do
     socket
-    |> assign(:page_title, "Listing Messages")
+#    |> assign(:page_title, "Listing Messages") # <-- delete here
     |> assign(:message, nil)
   end
 
